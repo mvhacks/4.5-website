540385d505d896571e0cfce3f684b25b42072099
Olivia Lai <olivialai886@gmail.com>
2021-07-05 14:02:51 -0700

    TBD to sponsors, add favicon

---
+++ src/components/Sponsors.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function Sponsors(props) {
+
+    return (
+        <div className='section'>
+            <h1>TBD...</h1>
+        </div>
+    );
+}
+
+export default Sponsors;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,36 +2,46 @@ import * as React from "react";
 import OrganizerCards from "../components/TheTeam/OrganizerCards";
 import PageSection from "../components/PageSection";
 import PhotoGallery from "../components/PhotoGallery";
+import Sponsors from "../components/Sponsors";
+import Favicon from 'react-favicon';
 
 import MVHacksLogo from "../images/MVHacksLogoMV.png";
 import Timeline from "../components/Timeline/Timeline";
+import faviconImg from "../images/SquareMVHacksLogo.png";
+
+
+
 
 // markup
 const IndexPage = () => {
+
     return (
 		<>
-		<svg xmlns="//www.w3.org/2000/svg" version="1.1" class="svg-filters" style={{display:'none'}}>
+		{/* <svg xmlns="//www.w3.org/2000/svg" version="1.1" class="svg-filters" style={{display:'none'}}>
 		<defs>
 			<filter id="marker-shape">
 			<feTurbulence type="fractalNoise" baseFrequency="0.001 0.06" numOctaves="1" result="turbulence" />
 			<feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="30" in="SourceGraphic" in2="warp" />
 			</filter>
 		</defs>
-		</svg>
+		</svg> */}
         <main>
             <title>MVHacks 4.5</title>
+			<Favicon url={faviconImg}/>
             <div className='topSection section' style={{minHeight: '80vh'}}>
 				<div className='l-header'>
-					<img src={MVHacksLogo} style={{ width: "clamp(20px, 80vw, 500px)", height: "auto", display:'' }} alt='MVHacks Logo'></img>
+					<img src={MVHacksLogo} style={{ width: "clamp(20px, 80vw, 500px)", height: "auto", display:''}} alt='MVHacks Logo'></img>
 					<h1 style={{ fontSize: "clamp(4em, 10vw, 6em)", display:'block' }} className='bold black'>
 						MVHacks 4.5
 					</h1>
 				</div>
 				<div className='r-header'>
-					<h1 style={{ fontSize: "clamp(2em, 3vw, 10em)", margin: '10px', marginBottom:'clamp(10px, 20vh, 100px)'}} className='gray'>
-						The hackathon for <span class='highlight'>everyone.</span><br/> (especially beginners)
+					<h1 style={{ fontSize: "clamp(2em, 3vw, 10em)", margin: '10px', marginBottom:'clamp(10px, 20vh, 100px)', lineHeight: '1.5em'}} className='darkGray'>
+						The hackathon for <span className='highlight black'>everyone.</span><br/> (especially beginners)
 					</h1>
-					<button className='applyButton'>Apply Now</button>
+					<form className='applyButtonForm' method="get" action="https://forms.gle/Em7kp6rXnLoaD4Eo9" target="_blank">
+    					<button className='applyButton' type="submit">Apply Now</button>
+					</form>
 				</div>
             </div>
             <PageSection title={"MVHacks"}> 
@@ -41,20 +51,23 @@ const IndexPage = () => {
 				<Timeline></Timeline>
 			</PageSection>
             <PageSection title={"Sponsors"}>
-
+				<Sponsors></Sponsors>
 			</PageSection>
             <PageSection title={"FAQS"}>
 				
 			</PageSection>
-
             <PageSection title={"The Team"}>
 				<OrganizerCards></OrganizerCards>
 			</PageSection>
         </main>
 		<footer className='center'>
 			<div className='center-col'>
-			<button className='applyButton inverse'>Apply Now</button>
-			<p className='white' style={{ paddingTop:'10px'}}>You will get in *</p>
+			
+			<form className='applyButtonForm center-col' method="get" action="https://forms.gle/Em7kp6rXnLoaD4Eo9" target="_blank">
+				<button className='applyButton inverse' type="submit">Apply Now</button>
+				<p className='white' style={{ paddingTop:'10px'}}>You will get in *</p>
+			</form>
+			
 			</div>
 			<p className='tiny' style={{justifySelf: "flex-end"}}>* Based on space available.</p>
 		</footer>
