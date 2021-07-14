35d5609b26247b4f59d1e03b455ada065e0da1b7
Olivia Lai <olivialai886@gmail.com>
2021-07-14 11:43:41 -0700

    Fix window node error, update styling

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -3,8 +3,10 @@ import * as React from "react";
 function Sponsors(props) {
     return (
         <div className='section sponsors'>
-            <h1 style={{ marginTop: "1em", marginBottom: "0.2em" }}>Coming soon!</h1>
-            <h2 style={{ marginBottom: "1em" }}>
+            <h1 style={{ marginTop: "1em", marginBottom: "0.2em", fontSize: '2.25em' }}>Coming soon!</h1>
+            <br></br>
+            <br></br>
+            <h2 style={{ marginBottom: "1em", fontSize: '1.5em' }}>
                 Interested in sponsoring MVHacks? Contact <a href='mailto:sonav@mvhacks.io'> sonav@mvhacks.io</a> and take a look at our{" "}
                 <a href='https://drive.google.com/file/d/1nYNdPKcjtIqK3pEunebXxK-GIHjW02pO/view?usp=sharing' rel='noreferrer' target='_blank'>
                     prospectus
