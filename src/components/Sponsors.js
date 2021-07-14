c01e09513b472b95f806574b55a6b85c9bde4fac
SonavAgarwal <42980809+SonavAgarwal@users.noreply.github.com>
2021-07-13 21:42:59 -0700

    fix sponsors section

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,10 +1,16 @@
-import * as React from 'react';
+import * as React from "react";
 
 function Sponsors(props) {
-
     return (
         <div className='section sponsors'>
-            <h1>TBD...</h1>
+            <h1 style={{ marginTop: "1em", marginBottom: "0.2em" }}>Coming soon!</h1>
+            <h2 style={{ marginBottom: "1em" }}>
+                Interested in sponsoring MVHacks? Contact <a href='mailto:sonav@mvhacks.io'> sonav@mvhacks.io</a> and take a look at our{" "}
+                <a href='https://drive.google.com/file/d/1nYNdPKcjtIqK3pEunebXxK-GIHjW02pO/view?usp=sharing' rel='noreferrer' target='_blank'>
+                    prospectus
+                </a>
+                !
+            </h2>
         </div>
     );
 }
