77b4228a98a84cfd5412120af8997c6e0defb7aa
SonavAgarwal <42980809+SonavAgarwal@users.noreply.github.com>
2021-07-13 21:48:10 -0700

    schedule subject to change

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,57 +1,52 @@
-import React, {useRef} from "react";
-import timeline from './timeline.json'
-import './timeline.css'
+import React, { useRef } from "react";
+import timeline from "./timeline.json";
+import "./timeline.css";
 import TimelineItem from "./TimelineItem";
-function Timeline(props)
-{
-	const timelineCenter = useRef();
-	const events = []
-	timeline.forEach(event => {
-
-		const out = {...event}
-		out.displayTime = event.time;
-		out.time = Date.parse('1/1/1970 '+event.time)
-		events.push(out)
-	});
-	events.sort((a, b) =>a.time-b.time);
-	function getMinDiffBetweenEvents()
-	{
-		let minDist = Number( events[2].time - events[0].time);
-		for (var i = 3; i<events.length; i++)
-		{
-			const diff = Number(events[i].time - events[i-2].time);
-			if (minDist>diff)
-			{
-				minDist = diff;
-			}
-		}
-		return minDist;
-	}
-	const itemHeight = 250;
-	const minDiff = getMinDiffBetweenEvents();
-	let runningHeight = itemHeight/4;
-	let runningTime = events[0].time;
-	const timeToTop = (time) =>{
-		runningHeight += itemHeight/2*Math.pow((time-runningTime)/minDiff, props.distort??1)
-		runningTime = time;
-		return runningHeight;
-	}
-	events.forEach((e, i)=>{
-		let props = e;
-		props.top = timeToTop(e.time) + 'px'
-		props.left = !!(i%2)
-		e.props = props;
-	})
-	runningHeight+=itemHeight*.75;
-	return (
-		<div className='timeline'>
-			<div ref={timelineCenter} className='timeline-center' style={{height: runningHeight}}>
-				{events.map((e, i)=>{
-					// return <TimelineItem key={i} title={e.title} displayTime={e.displayTime} top={timeToTop(e.time)-itemHeight/2+itemHeight/2} left={!!(i%2)}></TimelineItem>
-					return <TimelineItem key={i} {...e.props}></TimelineItem>
-				})}
-			</div>
-		</div>
-	)
+function Timeline(props) {
+    const timelineCenter = useRef();
+    const events = [];
+    timeline.forEach((event) => {
+        const out = { ...event };
+        out.displayTime = event.time;
+        out.time = Date.parse("1/1/1970 " + event.time);
+        events.push(out);
+    });
+    events.sort((a, b) => a.time - b.time);
+    function getMinDiffBetweenEvents() {
+        let minDist = Number(events[2].time - events[0].time);
+        for (var i = 3; i < events.length; i++) {
+            const diff = Number(events[i].time - events[i - 2].time);
+            if (minDist > diff) {
+                minDist = diff;
+            }
+        }
+        return minDist;
+    }
+    const itemHeight = 250;
+    const minDiff = getMinDiffBetweenEvents();
+    let runningHeight = itemHeight / 4;
+    let runningTime = events[0].time;
+    const timeToTop = (time) => {
+        runningHeight += (itemHeight / 2) * Math.pow((time - runningTime) / minDiff, props.distort ?? 1);
+        runningTime = time;
+        return runningHeight;
+    };
+    events.forEach((e, i) => {
+        let props = e;
+        props.top = timeToTop(e.time) + "px";
+        props.left = !!(i % 2);
+        e.props = props;
+    });
+    runningHeight += itemHeight * 0.75;
+    return (
+        <div className='timeline'>
+            <div ref={timelineCenter} className='timeline-center' style={{ height: runningHeight }}>
+                {events.map((e, i) => {
+                    // return <TimelineItem key={i} title={e.title} displayTime={e.displayTime} top={timeToTop(e.time)-itemHeight/2+itemHeight/2} left={!!(i%2)}></TimelineItem>
+                    return <TimelineItem key={i} {...e.props}></TimelineItem>;
+                })}
+            </div>
+        </div>
+    );
 }
 export default Timeline;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,85 +3,95 @@ import OrganizerCards from "../components/TheTeam/OrganizerCards";
 import PageSection from "../components/PageSection";
 import PhotoGallery from "../components/PhotoGallery";
 import Sponsors from "../components/Sponsors";
-import Favicon from 'react-favicon';
+import Favicon from "react-favicon";
 import Faqs from "../components/faq";
-import '../styles/navbar.css';
-import navbar from './navbar.js';
+import "../styles/navbar.css";
+import navbar from "./navbar.js";
 import MVHacksLogo from "../images/MVHacksLogoMV.png";
 import Timeline from "../components/Timeline/Timeline";
 import faviconImg from "../images/SquareMVHacksLogo.png";
 
 // markup
 const IndexPage = () => {
-	// run navbar() after this element is rendered
-	React.useEffect(() => {
-		navbar();
-	}, []);
+    // run navbar() after this element is rendered
+    React.useEffect(() => {
+        navbar();
+    }, []);
     return (
-		<>
-		<svg xmlns="//www.w3.org/2000/svg" version="1.1" className="svg-filters" style={{display:'none'}}>
-		<defs>
-			<filter id="marker-shape">
-			<feTurbulence type="fractalNoise" baseFrequency="0.001 0.06" numOctaves="1" result="turbulence" />
-			<feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="30" in="SourceGraphic" in2="warp" />
-			</filter>
-		</defs>
-		</svg>
-        <main>
-		<Favicon url={faviconImg}/>
-			<title>MVHacks 4.5</title>
-			<nav>
-				<button id="pancake">|||</button>
-				<a href="#MVHacks">Photo Gallery</a>
-				<a href="#Schedule">Schedule</a>
-				<a href="#Sponsors">Sponsors</a>
-				<a href="#FAQs">FAQs</a>
-				<a href="#The-Team">The Team</a>
-			</nav>
-            <div className='topSection section' style={{minHeight: '80vh'}}>
-				<div className='l-header'>
-					<img src={MVHacksLogo} style={{ width: "clamp(20px, 80vw, 500px)", height: "auto", display:''}} alt='MVHacks Logo'></img>
-					<h1 style={{ fontSize: "clamp(4em, 10vw, 6em)", display:'block' }} className='bold black'>
-						MVHacks 4.5
-					</h1>
-				</div>
-				<div className='r-header'>
-					<h1 style={{ fontSize: "clamp(2em, 3vw, 10em)", margin: '10px', marginBottom:'clamp(8px, 10vh, 80px)', lineHeight: '1.5em'}} className='darkGray'>
-						The hackathon for <span className='highlight black'>everyone.</span><br/> (especially beginners)
-					</h1>
-					<form className='applyButtonForm' method="get" action="https://forms.gle/Em7kp6rXnLoaD4Eo9" target="_blank">
-    					<button className='applyButton' type="submit">Apply Now</button>
-					</form>
-				</div>
-            </div>
-            <PageSection title={"MVHacks"}>
-				<PhotoGallery />
-			</PageSection>
-            <PageSection title={"Schedule"}>
-                <Timeline distort={.5}></Timeline>
-            </PageSection>
-            <PageSection title={"Sponsors"}>
-				<Sponsors></Sponsors>
-			</PageSection>
-            <PageSection title={"FAQs"}>
-              <Faqs/>
-			</PageSection>
-            <PageSection title={"The Team"}>
-				<OrganizerCards></OrganizerCards>
-			</PageSection>
-        </main>
-		<footer className='center'>
-			<div className='center-col'>
-
-			<form className='applyButtonForm center-col' method="get" action="https://forms.gle/Em7kp6rXnLoaD4Eo9" target="_blank">
-				<button className='applyButton inverse' type="submit">Apply Now</button>
-				<p className='white' style={{ paddingTop:'10px'}}>You will get in *</p>
-			</form>
-
-			</div>
-			<p className='tiny' style={{justifySelf: "flex-end"}}>* Based on space available.</p>
-		</footer>
-		</>
+        <>
+            <svg xmlns='//www.w3.org/2000/svg' version='1.1' className='svg-filters' style={{ display: "none" }}>
+                <defs>
+                    <filter id='marker-shape'>
+                        <feTurbulence type='fractalNoise' baseFrequency='0.001 0.06' numOctaves='1' result='turbulence' />
+                        <feDisplacementMap xChannelSelector='R' yChannelSelector='G' scale='30' in='SourceGraphic' in2='warp' />
+                    </filter>
+                </defs>
+            </svg>
+            <main>
+                <Favicon url={faviconImg} />
+                <title>MVHacks 4.5</title>
+                <nav>
+                    <button id='pancake'>|||</button>
+                    <a href='#MVHacks'>Photo Gallery</a>
+                    <a href='#Schedule'>Schedule</a>
+                    <a href='#Sponsors'>Sponsors</a>
+                    <a href='#FAQs'>FAQs</a>
+                    <a href='#The-Team'>The Team</a>
+                </nav>
+                <div className='topSection section' style={{ minHeight: "80vh" }}>
+                    <div className='l-header'>
+                        <img src={MVHacksLogo} style={{ width: "clamp(20px, 80vw, 500px)", height: "auto", display: "" }} alt='MVHacks Logo'></img>
+                        <h1 style={{ fontSize: "clamp(4em, 10vw, 6em)", display: "block" }} className='bold black'>
+                            MVHacks 4.5
+                        </h1>
+                    </div>
+                    <div className='r-header'>
+                        <h1
+                            style={{ fontSize: "clamp(2em, 3vw, 10em)", margin: "10px", marginBottom: "clamp(8px, 10vh, 80px)", lineHeight: "1.5em" }}
+                            className='darkGray'>
+                            The hackathon for <span className='highlight black'>everyone.</span>
+                            <br /> (especially beginners)
+                        </h1>
+                        <form className='applyButtonForm' method='get' action='https://forms.gle/Em7kp6rXnLoaD4Eo9' target='_blank'>
+                            <button className='applyButton' type='submit'>
+                                Apply Now
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <PageSection title={"MVHacks"}>
+                    <PhotoGallery />
+                </PageSection>
+                <PageSection title={"Schedule"}>
+                    <h3 style={{ marginTop: "1em", paddingLeft: "2em" }}>*The schedule is subject to change</h3>
+                    <Timeline distort={0.5}></Timeline>
+                </PageSection>
+                <PageSection title={"Sponsors"}>
+                    <Sponsors></Sponsors>
+                </PageSection>
+                <PageSection title={"FAQs"}>
+                    <Faqs />
+                </PageSection>
+                <PageSection title={"The Team"}>
+                    <OrganizerCards></OrganizerCards>
+                </PageSection>
+            </main>
+            <footer className='center'>
+                <div className='center-col'>
+                    <form className='applyButtonForm center-col' method='get' action='https://forms.gle/Em7kp6rXnLoaD4Eo9' target='_blank'>
+                        <button className='applyButton inverse' type='submit'>
+                            Apply Now
+                        </button>
+                        <p className='white' style={{ paddingTop: "10px" }}>
+                            You will get in *
+                        </p>
+                    </form>
+                </div>
+                <p className='tiny' style={{ justifySelf: "flex-end" }}>
+                    * Based on space available.
+                </p>
+            </footer>
+        </>
     );
 };
 
